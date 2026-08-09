a17c1f9f952be35849287e389c2fe73724f80c28
blinkywink <thetrueblue27@proton.me>
2026-08-08 19:42:34 -0700

    dd

--- src/games/pricecheck/index.ts
+++
@@ -1,4 +0,0 @@
-export { PriceCheckGame } from "./PriceCheckGame";
-export { PRICE_CHECK_CONFIG } from "./config";
-export { comboCost, formatCash } from "./costs";
-export { loadBestScores } from "./scoring";
